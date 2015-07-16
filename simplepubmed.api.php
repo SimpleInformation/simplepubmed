bf5aac9b3fdef42931b952dcd9b82c0699fcafba
Mark McIntosh <mark@simpleinformation.com>
2015-07-16 10:08:56 -0400

    fixing param coding standard issues

--- simplepubmed.api.php
+++ simplepubmed.api.php
@@ -11,9 +11,9 @@
  * The hook will only be triggered if the resolved data has changed and
  * not the value set by the user.
  *
- * @param $publications
+ * @param array $publications
  *   An associative array of the form (id => title) containing new publications.
- * @param $nid
+ * @param string $nid
  *   The Node id of the field the given field is associated to.
  */
 function hook_simplepubmed_field_update($publications, $nid) {
